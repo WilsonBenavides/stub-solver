0fd42edd07bcea2e53cf774527b44d180a2bd7d3
ing wilson <ing.wilson.benavides@gmail.com>
2019-03-02 22:45:53 -0500

    v0.3 Parameterized Test for calculateResCircle

--- src/test/java/com/willy/smith/test/SmithChartTest.java
+++ src/test/java/com/willy/smith/test/SmithChartTest.java
@@ -4,25 +4,51 @@ import static org.hamcrest.Matchers.closeTo;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.equalTo;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Map;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
 
 import com.willy.smith.chart.SmithChart;
 
+@RunWith(Parameterized.class)
 public class SmithChartTest {
 	
+	@Parameter(0)
+	public Double rl;
+	@Parameter(1)
+	public Double xc;
+	@Parameter(2)
+	public Double yc;
+	@Parameter(3)
+	public Double radius;
+	
+	//creates the test data
+	@Parameters
+	public static Collection<Object[]> data() {
+		Object[][] data = new Object[][] {
+			{0.1, 0.0909090909, 0.0, 0.9090909090}, {0.5, 0.3333333334, 0.0, 0.6666666667}
+		};
+		return Arrays.asList(data);
+	}
+
 	@Test
 	public void testCalculateResCircle() {
 		SmithChart smithChart = new SmithChart();
 
-		Map<String, Double> map = smithChart.calculateResCircle(0.5);
-		
-		assertThat(map.get("x-center"), is(closeTo(0.3333333334, 0.0000000001)));
-		assertThat(map.get("y-center"), is(equalTo(0.0)));
-		assertThat(map.get("radius"), is(closeTo(0.6666666667, 0.0000000001)));
-		
+		double delta = 0.0000000001;
+		Map<String, Double> map = smithChart.calculateResCircle(rl);
+
+		assertThat(map.get("x-center"), is(closeTo(xc, delta)));
+		assertThat(map.get("y-center"), is(equalTo(yc)));
+		assertThat(map.get("radius"), is(closeTo(radius, delta)));
+
 	}
 
 }
