c482604cf00f45ad4090ee1808f76cec3af22be8
ing wilson <ing.wilson.benavides@gmail.com>
2019-03-02 19:53:21 -0500

    v0.2 Map return calculateSmithCircle

--- src/main/java/com/willy/smith/chart/SmithChart.java
+++ src/main/java/com/willy/smith/chart/SmithChart.java
@@ -1,6 +1,7 @@
 package com.willy.smith.chart;
 
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SmithChart {
 
@@ -9,16 +10,16 @@ public class SmithChart {
 	 * @param rl standardized load resistance
 	 * @return the function calculates the value of the radius and the center of the resistance circle from the rl value
 	 */
-	public ArrayList<Double> calculateResCircle(double rl) {
-		ArrayList<Double> result = new ArrayList<Double>();
+	public Map<String, Double> calculateResCircle(double rl) {
+		Map<String, Double> result = new HashMap<String, Double>();
 
 		double xc = rl / (1 + rl); // x-center
 		double yc = 0; // y-center
 		double radius = 1 / (1 + rl); // radio
 
-		result.add(xc);
-		result.add(yc);
-		result.add(radius);
+		result.put("x-center", xc);
+		result.put("y-center", yc);
+		result.put("radius", radius);
 
 		return result;
 	}
--- src/test/java/com/willy/smith/test/SmithChartTest.java
+++ src/test/java/com/willy/smith/test/SmithChartTest.java
@@ -2,6 +2,9 @@ package com.willy.smith.test;
 
 import static org.hamcrest.Matchers.closeTo;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.equalTo;
+
+import java.util.Map;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import org.junit.Test;
@@ -13,10 +16,12 @@ public class SmithChartTest {
 	@Test
 	public void testCalculateResCircle() {
 		SmithChart smithChart = new SmithChart();
+
+		Map<String, Double> map = smithChart.calculateResCircle(0.5);
 		
-		double radius = smithChart.calculateResCircle(0.5).get(2);
-		
-		assertThat(radius, is(closeTo(0.6666666667, 0.0000000001)));
+		assertThat(map.get("x-center"), is(closeTo(0.3333333334, 0.0000000001)));
+		assertThat(map.get("y-center"), is(equalTo(0.0)));
+		assertThat(map.get("radius"), is(closeTo(0.6666666667, 0.0000000001)));
 		
 	}
 
