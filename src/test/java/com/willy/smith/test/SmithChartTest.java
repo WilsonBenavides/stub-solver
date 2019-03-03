07f58acb222105bcb0c7760151b3210c2cbbfa77
IngWilsonBenavides <ing.wilson.benavides@gmail.com>
2019-03-02 23:18:12 -0500

    v0.4 Before Junit anotation and more values

--- src/test/java/com/willy/smith/test/SmithChartTest.java
+++ src/test/java/com/willy/smith/test/SmithChartTest.java
@@ -9,6 +9,8 @@ import java.util.Collection;
 import java.util.Map;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -19,7 +21,9 @@ import com.willy.smith.chart.SmithChart;
 
 @RunWith(Parameterized.class)
 public class SmithChartTest {
-	
+
+	private SmithChart smithChart;
+
 	@Parameter(0)
 	public Double rl;
 	@Parameter(1)
@@ -28,22 +32,26 @@ public class SmithChartTest {
 	public Double yc;
 	@Parameter(3)
 	public Double radius;
-	
-	//creates the test data
+
+	// creates the test data
 	@Parameters
 	public static Collection<Object[]> data() {
-		Object[][] data = new Object[][] {
-			{0.1, 0.0909090909, 0.0, 0.9090909090}, {0.5, 0.3333333334, 0.0, 0.6666666667}
-		};
+		Object[][] data = new Object[][] { { 0.1, 0.0909090909, 0.0, 0.9090909090 },
+				{ 0.2, 0.1666666667, 0.0, 0.8333333334 }, { 0.3, 0.2307692308, 0.0, 0.7692307692 },
+				{ 0.4, 0.2857142857, 0.0, 0.7142857143 }, { 0.5, 0.3333333334, 0.0, 0.6666666667 } };
 		return Arrays.asList(data);
+	}
+
+	@Before
+	public void initObjects() {
+		smithChart = new SmithChart();
 	}
 
 	@Test
 	public void testCalculateResCircle() {
-		SmithChart smithChart = new SmithChart();
 
-		double delta = 0.0000000001;
 		Map<String, Double> map = smithChart.calculateResCircle(rl);
+		double delta = 0.0000000001;
 
 		assertThat(map.get("x-center"), is(closeTo(xc, delta)));
 		assertThat(map.get("y-center"), is(equalTo(yc)));
