376d3748aabfdd6e9c0357dbf6796fcbb1ef3088
ing wilson <ing.wilson.benavides@gmail.com>
2019-03-03 02:38:41 -0500

    v0.6 Add GUI JavaFX maven dependency

---
+++ src/main/java/com/willy/smith/main/StubSolverMain.java
@@ -0,0 +1,31 @@
+package com.willy.smith.main;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class StubSolverMain extends Application {
+
+	private static final double WIDTH = 100.0;
+	private static final double HEIGHT = WIDTH * 0.618;
+	private static final double SCALE = 10.0;
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	public void start(Stage primaryStage) throws Exception {
+		StackPane root = new StackPane();
+		primaryStage.setScene(createScene(root, WIDTH * SCALE, HEIGHT * SCALE));
+		primaryStage.setTitle("Stub Solver");
+		primaryStage.show();
+	}
+
+	private Scene createScene(Pane root, double width, double height) {
+		Scene scene = new Scene(root, width, height);
+		return scene;
+	}
+
+}
