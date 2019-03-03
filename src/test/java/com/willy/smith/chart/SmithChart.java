93ecacb2474477545876a4f78928012b84953fbd
ing wilson <ing.wilson.benavides@gmail.com>
2019-03-02 19:01:50 -0500

    v0.1 calculateResCircle Success test

--- src/test/java/com/willy/smith/chart/SmithChart.java
+++ src/test/java/com/willy/smith/chart/SmithChart.java
@@ -4,9 +4,23 @@ import java.util.ArrayList;
 
 public class SmithChart {
 
-	public ArrayList<Double> calculateResCircle(double d) {
-		
-		return null;
+	/**
+	 * 
+	 * @param rl standardized load resistance
+	 * @return the function calculates the value of the radius and the center of the resistance circle from the rl value
+	 */
+	public ArrayList<Double> calculateResCircle(double rl) {
+		ArrayList<Double> result = new ArrayList<Double>();
+
+		double xc = rl / (1 + rl); // x-center
+		double yc = 0; // y-center
+		double radius = 1 / (1 + rl); // radio
+
+		result.add(xc);
+		result.add(yc);
+		result.add(radius);
+
+		return result;
 	}
 
 }
